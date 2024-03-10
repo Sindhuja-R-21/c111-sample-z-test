b52d61feca77d790523c4e6cee96ae8d76eb70aa
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2024-03-10 22:56:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+import csv
+
+
+
+
+
+
+
+
+
+
+
+# # finding the mean of THE STUDENTS WHO GAVE EXTRA TIME TO MATH LAB and plotting on graph
+
+
+fig = ff.create_distplot([mean_list], ["student marks"], show_hist=False)
+fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 0.17], mode="lines", name="MEAN"))
+fig.add_trace(go.Scatter(x=[mean_of_sample1, mean_of_sample1], y=[0, 0.17], mode="lines", name="MEAN OF STUDENTS WHO HAD MATH LABS"))
+fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 1 END"))
+fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
+fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
+fig.show()
+
+
